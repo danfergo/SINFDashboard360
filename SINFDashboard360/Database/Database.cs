88bd4de10194ebb9b7fcdde4c9ed3a55ce8142df
Daniel Gomes <up201306839@fe.up.pt>
2015-11-16 23:28:06 +0000

    addning database.cs and models.department.cs

---
+++ SINFDashboard360/Database/Database.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SINFDashboard360.Database
+{
+    public class Database
+    {
+    }
+}
---
+++ SINFDashboard360/Models/Departament.cs
@@ -0,0 +1,15 @@
+﻿using Interop.StdBE800;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SINFDashboard360.Models
+{
+    public class Departament
+    {
+        public string department_id;
+        public string description;
+        public List<Funcionario> employees = new List<Funcionario>();
+    }
+}
