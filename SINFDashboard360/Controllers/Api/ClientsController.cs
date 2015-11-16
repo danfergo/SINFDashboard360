63c61d507d780975bf3f2e1942bb0224aca5bdde
Daniel Gomes <up201306839@fe.up.pt>
2015-11-16 23:24:43 +0000

    addning sales and clients controllers

---
+++ SINFDashboard360/Controllers/Api/ClientsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SINFDashboard360.Lib_Primavera.Model;
+using Interop.StdBE800;
+using SINFDashboard360.Lib_Primavera;
+
+
+namespace SINFDashboard360.Controllers
+{
+    public class ClientsController : ApiController
+    {
+
+        // GET: /Clientes/
+
+        public IEnumerable<Models.Client> Get()
+        {
+            return Pri_Bridge.Clients.ListaClientes();
+        }
+
+        [HttpGet]
+        // GET api/cliente/5    
+        public Models.Client Get(string id)
+        {
+            System.Diagnostics.Debug.WriteLine(id);
+            Models.Client cliente = Pri_Bridge.Clients.GetCliente(id);
+            if (cliente == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+
+            }
+            else
+            {
+                return cliente;
+            }
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Lib_Primavera.Model.Factura> GetClientInvoices(string id)
+        {
+
+            return Pri_Bridge.Clients.getClientInvoices(id);
+        }
+       
+    }
+}
---
+++ SINFDashboard360/Controllers/Api/SalesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Interop.ErpBS800;
+using Interop.StdPlatBS800;
+using Interop.StdBE800;
+using Interop.GcpBE800;
+using ADODB;
+using Interop.IGcpBS800;
+using SINFDashboard360.Lib_Primavera.Model;
+using SINFDashboard360.Lib_Primavera;
+ 
+namespace SINFDashboard360.Controllers
+{
+    public class SalesController : ApiController
+    {
+        //// GET api/sales
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        public IEnumerable<DocVenda> Get(string min_date, string max_date)
+        {
+            return Pri_Bridge.Sales.ListaFiltradaPorDatas(min_date, max_date);
+        }
+
+        // GET api/sales/5
+        public DocVenda Get(int id)
+        {
+            string numdoc = id.ToString();
+            return Pri_Bridge.Purchases.Encomenda_Get(numdoc);
+        }
+
+    }
+}
