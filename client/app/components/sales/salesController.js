f053bb95652d1ae919fa188ac2fd789bd951193d
Daniel Gomes <up201306839@fe.up.pt>
2015-11-27 02:09:58 +0000

    sales. update bower (to old version - exists bug in recent version angular material).

--- client/app/components/sales/salesController.js
+++ client/app/components/sales/salesController.js
@@ -1,4 +1,27 @@
 angular.module('dash-sales').
-controller('salesController', ['$scope', function ($scope) {
-		$scope.$parent.pageTitle = "Vendas";
+controller('salesController', ['$scope','Sale','$filter', function ($scope, Sale,$filter) {
+	$scope.$parent.pageTitle = "Vendas";
+
+	$scope.today = new Date(); 
+	$scope.maxDate = new Date(); 
+	$scope.minDate = new Date();
+	$scope.minDate.setDate($scope.maxDate.getDate()-90);
+
+
+	$scope.filterResults = function(){
+		$scope.sales = null;
+		Sale.query({
+					min_date: $filter('date')($scope.minDate,'dd-MM-yyyy'),
+				 	max_date: $filter('date')($scope.maxDate,'dd-MM-yyyy') 
+					}, 
+		function(data){
+			$scope.sales = data;
+		});
+	}
+	
+	$scope.filterResults();
+
+	$scope.toggle = function(event){
+		$(event.currentTarget).toggleClass('active');
+	}
 }]);
