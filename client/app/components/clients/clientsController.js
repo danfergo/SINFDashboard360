d23a01796fedda5d7fab0e16caccd4f5b0e9cccc
Diogo Gomes <up201106586@fe.up.pt>
2015-12-17 22:15:38 +0000

    Finished clients.

--- client/app/components/clients/clientsController.js
+++ client/app/components/clients/clientsController.js
@@ -1,22 +1,38 @@
 angular.module('dash-clients').
-controller('clientsController', ['$scope','Client', function ($scope, Client) {
+controller('clientsController', ['$scope','Client', '$filter', function ($scope, Client, $filter) {
 	$scope.$parent.pageTitle = "Clientes";
 	$scope.clients = null;
 
 	Client.query(function(data){
-		$scope.clients = data;
+
+    data.forEach(function(element){
+      element.ValorTotal = element.ValorTotal > 0 ? Math.round(element.ValorTotal * 100) / 100 : -Math.round(element.ValorTotal * 100) / 100;
+      element.ValorPendente = element.ValorPendente > 0 ? Math.round(element.ValorPendente * 100) / 100 : -Math.round(element.ValorPendente * 100) / 100;
+    });
+
+    $scope.clients = $filter('orderBy')(data, 'ValorTotal', true);
+
+    var debtors = $filter('orderBy')(data, 'ValorPendente', true);
+    var buyers = $scope.clients;
+
+    $scope.series = ['Series A'];
+
+    $scope.labelsbuyers = [];
+    $scope.dataBuyers = [[]];
+
+    $scope.labelsDebtors = [];
+    $scope.dataDebtors = [[]];
+
+    for(var i = 0; i < 6; i++){
+      $scope.labelsbuyers.push(buyers[i].name.replace(/[^A-Z]/g, ''));
+      $scope.dataBuyers[0].push(buyers[i].ValorTotal);
+      $scope.labelsDebtors.push(debtors[i].name.replace(/[^A-Z]/g, ''));
+      $scope.dataDebtors[0].push(debtors[i].ValorPendente);
+    }
 	});
-		
-		/*
-	$scope.toggle = function(event){
-		$(event.currentTarget).toggleClass('active');
-	}*/
 
-  $scope.labels = ['João', 'Pedro', 'Maria', 'Joana', 'António', 'Rui', 'Ricardo'];
-  $scope.series = ['Series A'];
+  $scope.formatNumber = function(i) {
+    return Math.round(i * 100)/100;
+  }
 
-  $scope.data = [
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
-         
 }]);
