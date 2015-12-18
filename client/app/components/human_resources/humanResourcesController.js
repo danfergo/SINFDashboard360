050fae548c9b7f65a39dac1431d1ca7a0f179bac
Diogo Gomes <up201106586@fe.up.pt>
2015-12-18 00:01:35 +0000

    Fixed Human Resources graphs

--- client/app/components/human_resources/humanResourcesController.js
+++ client/app/components/human_resources/humanResourcesController.js
@@ -1,23 +1,37 @@
 angular.module('dash-humanResources').
 controller('humanResourcesController', ['$scope', 'Deparment', function ($scope, Deparment) {
-	$scope.$parent.pageTitle = "Recursos Humanos";
-	$scope.departments = null;
+  $scope.$parent.pageTitle = "Recursos Humanos";
+  $scope.departments = null;
 
-	Deparment.query(function(data){
-		$scope.departments = data;
-	});
+  $scope.getTotalSalaries = function (department) {
+    var total = 0;
+    for (var e in department.employees) {
+      total += department.employees[e].salary;
+    }
+    return total;
+  };
 
-	$scope.getTotalSalaries = function(department){
-		var total = 0;
-		for(var e in department.employees){
-			total  += department.employees[e].salary;
-		}
-		return total;
-	}
+  Deparment.query(function (data) {
+    $scope.departments = data;
 
+    $scope.labelsSalaries = [];
+    $scope.dataSalaries = [];
 
-	$scope.toggle = function(event){
-		$(event.currentTarget).toggleClass('active');
-	}
+    $scope.dataCollaborators = [];
+    $scope.labelsCollaborators = [];
+
+    for (var i = 0; i < data.length; i++) {
+      $scope.dataSalaries.push($scope.getTotalSalaries(data[i]));
+      $scope.labelsSalaries.push(data[i].description);
+
+      $scope.dataCollaborators.push(data[i].employees.length);
+      $scope.labelsCollaborators.push(data[i].description);
+
+    }
+  });
+
+  $scope.toggle = function (event) {
+    $(event.currentTarget).toggleClass('active');
+  };
 
 }]);
